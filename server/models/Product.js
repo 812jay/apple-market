09deb05db9c3638bfb0bfc09f356f5131552a99d
812jay <yongje812@gmail.com>
2021-05-02 13:39:08 +0900

    Create Product Schema

---
+++ server/models/Product.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = mongoose.Schema({
+    writer: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    title: {
+        type: String,
+        maxlength: 50
+    },
+    description: {
+        type: String
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    images: {
+        type: Array,
+        default: []
+    },
+    sold: {
+        type: Boolean,
+        default: false
+    },
+    views: {
+        type: Number,
+        default: 0
+    }
+}, {timeStamp: true});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = { Product }
