f2ad5e4b9ed0365abc4baff4d6c277a098a435eb
812jay <yongje812@gmail.com>
2021-05-20 14:47:54 +0900

    Create Upload Data

---
+++ client/src/components/views/UploadProductPage/Sections/Data.js
@@ -0,0 +1,14 @@
+const sort = [
+    {key: 1, value: '디지털/가전'}, 
+    {key: 2, value: '가구/인테리어'}, 
+    {key: 3, value: '유아동/유아도서'}, 
+    {key: 4, value: '패션/잡화'}, 
+    {key: 5, value: '게임/취미'}, 
+    {key: 6, value: '뷰티/미용'}, 
+    {key: 7, value: '반려동물'},
+    {key: 8, value: '도서/티켓/음반'},
+    {key: 9, value: '스포츠/레저'},
+    {key: 10, value: '기타'}
+];
+
+export { sort }
--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -4,19 +4,8 @@ import { Avatar, Button, Form, Input } from 'antd';
 import TextArea from 'antd/lib/input/TextArea';
 import FileUpload from '../../utils/FileUpload';
 import axios from 'axios';
+import { sort } from './Sections/Data';
 
-const sortList = [
-    {key: 1, value: '디지털/가전'}, 
-    {key: 2, value: '가구/인테리어'}, 
-    {key: 3, value: '유아동/유아도서'}, 
-    {key: 4, value: '패션/잡화'}, 
-    {key: 5, value: '게임/취미'}, 
-    {key: 6, value: '뷰티/미용'}, 
-    {key: 7, value: '반려동물'},
-    {key: 8, value: '도서/티켓/음반'},
-    {key: 9, value: '스포츠/레저'},
-    {key: 10, value: '기타'}
-];
 
 function UploadProductPage(props) {
     const [Images, setImages] = useState('');
@@ -102,7 +91,7 @@ function UploadProductPage(props) {
                     <label>분류</label>
                     <br />
                     <select onChange={sortChangeHandler}>
-                        {sortList.map(item => (
+                        {sort.map(item => (
                             <option key={item.key} value={item.key}>{item.value}</option>
                         ))}
                     </select>
