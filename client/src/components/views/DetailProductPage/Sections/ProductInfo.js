7345735d959b4fbeb340b514ba28f57762136723
812jay <yongje812@gmail.com>
2021-05-22 03:33:33 +0900

    Modify Sort Datas

--- client/src/components/views/DetailProductPage/Sections/ProductInfo.js
+++ client/src/components/views/DetailProductPage/Sections/ProductInfo.js
@@ -53,13 +53,17 @@ function ProductInfo(props) {
         if(value === 1) sort = '디지털/가전';
         if(value === 2) sort = '가구/인테리어';
         if(value === 3) sort = '유아동/유아도서';
-        if(value === 4) sort = '패션/잡화';
-        if(value === 5) sort = '게임/취미';
-        if(value === 6) sort = '뷰티/미용';
-        if(value === 7) sort = '반려동물';
-        if(value === 8) sort = '도서/티켓/음반';
-        if(value === 9) sort = '스포츠/레저';
-        if(value === 10) sort = '기타';
+        if(value === 4) sort = '생활/가공식품';
+        if(value === 5) sort = '스포츠/레저';
+        if(value === 6) sort = '여성패션/잡화';
+        if(value === 7) sort = '남성패션/잡화';
+        if(value === 8) sort = '게임/취미';
+        if(value === 9) sort = '뷰티/미용';
+        if(value === 10) sort = '반려동물용품';
+        if(value === 11) sort = '도서/티켓/음반';
+        if(value === 12) sort = '식물';
+        if(value === 13) sort = '기타';
+        if(value === 14) sort = '삽니다';
         setSort(sort);
     };
 
--- client/src/components/views/UploadProductPage/Sections/Data.js
+++ client/src/components/views/UploadProductPage/Sections/Data.js
@@ -2,13 +2,17 @@ const sort = [
     {key: 1, value: '디지털/가전'}, 
     {key: 2, value: '가구/인테리어'}, 
     {key: 3, value: '유아동/유아도서'}, 
-    {key: 4, value: '패션/잡화'}, 
-    {key: 5, value: '게임/취미'}, 
-    {key: 6, value: '뷰티/미용'}, 
-    {key: 7, value: '반려동물'},
-    {key: 8, value: '도서/티켓/음반'},
-    {key: 9, value: '스포츠/레저'},
-    {key: 10, value: '기타'}
+    {key: 4, value: '생활/가공식품'}, 
+    {key: 5, value: '스포츠/레저'}, 
+    {key: 6, value: '여성패션/잡화'}, 
+    {key: 7, value: '남성패션/잡화'},
+    {key: 8, value: '게임/취미'},
+    {key: 9, value: '뷰티/미용'},
+    {key: 10, value: '반려동물용품'},
+    {key: 11, value: '도서/티켓/음반'},
+    {key: 12, value: '식물'},
+    {key: 13, value: '기타'},
+    {key: 14, value: '삽니다'}
 ];
 
 export { sort }
